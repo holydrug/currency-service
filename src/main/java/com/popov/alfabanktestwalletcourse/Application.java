bf2643f7a68a5f393ceeeb6fb5fdd6b7fc7b2956
holydrug <nonprofitxz@gmail.com>
2022-06-09 16:22:02 +0300

    deleted jpa imports

--- src/main/java/com/popov/alfabanktestwalletcourse/Application.java
+++ src/main/java/com/popov/alfabanktestwalletcourse/Application.java
@@ -3,9 +3,6 @@ package com.popov.alfabanktestwalletcourse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
